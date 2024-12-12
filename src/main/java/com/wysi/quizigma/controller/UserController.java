c2886c5875f7126449b5bce906f1abd07ac70be3
khoibut <khoibut09@gmail.com>
2024-12-12 21:42:28 +0700

    Refactor user authentication and registration to use UserDTO; add username support in UserRepository.

--- src/main/java/com/wysi/quizigma/controller/UserController.java
+++ src/main/java/com/wysi/quizigma/controller/UserController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wysi.quizigma.DTO.UserDTO;
 import com.wysi.quizigma.JwtUtil;
-import com.wysi.quizigma.model.User;
 import com.wysi.quizigma.service.UserService;
 
 @RestController
@@ -20,11 +20,9 @@ import com.wysi.quizigma.service.UserService;
 public class UserController {
 
     private final UserService userService;
-    private final JwtUtil jwtUtil;
 
     public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
-        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping("/")
@@ -33,24 +31,29 @@ public class UserController {
     }
 
     @PostMapping("/auth")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody User user) {
+    public ResponseEntity<Map<String, Object>> login(@RequestBody UserDTO user) {
         Map<String, Object> response = new HashMap<>();
         try {
-            userService.authenticate(user.getEmail(), user.getPassword());
+            String token = userService.authenticate(user.getEmail(), user.getPassword());
+            response.put("token", token);
         } catch (IllegalArgumentException e) {
             response.put("error", e.getMessage());
             return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
         }
-        response.put("token", jwtUtil.generateToken(user));
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/")
-    public ResponseEntity<Map<String, Object>> addNewUser(@RequestBody User user) {
-        userService.addNewUser(user.getUsername(), user.getEmail(), user.getPassword());
-        Map<String, Object> response = new HashMap<>();
-        response.put("token", jwtUtil.generateToken(user));
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    public ResponseEntity<Map<String, Object>> addNewUser(@RequestBody UserDTO user) {
+        try {
+            String token = userService.addNewUser(user);
+            Map<String, Object> response = new HashMap<>();
+            response.put("token", token);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
     }
-
 }
