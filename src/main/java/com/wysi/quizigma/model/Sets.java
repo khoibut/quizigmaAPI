7213fefc2a1d419edac4397058d2ec17d92d6fc5
khoibut <khoibut09@gmail.com>
2024-12-12 21:11:23 +0700

    Add Sets entity with fields for id, name, description, and image

---
+++ src/main/java/com/wysi/quizigma/model/Sets.java
@@ -0,0 +1,68 @@
+package com.wysi.quizigma.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sets")
+public class Sets {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+    
+    @Column(name = "image", columnDefinition = "BYTEA")
+    private byte[] image;
+
+    //constructor for hibernate
+    public Sets() {
+    }
+
+    public Sets(String name, String description, byte[] image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
