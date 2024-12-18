e4a647dbc7d062e42cc694c759388bcaee3386ef
khoibut <khoibut09@gmail.com>
2024-12-18 11:50:41 +0700

    Add JWT authentication and environment configuration; integrate security filter

---
+++ src/main/java/com/wysi/quizigma/JwtAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.wysi.quizigma;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        System.out.println("Request URL: " + request.getRequestURI());
+        System.out.println("Authorization Header: " + request.getHeader("Authorization"));
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            try {
+                jwtUtil.isValid(token);
+                String userId = jwtUtil.getUserId(token).toString();
+                System.out.println(token);
+                System.out.println("User ID: " + userId);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                System.out.println("Authentication set: " + authentication);
+            } catch (JwtException | IllegalArgumentException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        } else {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
