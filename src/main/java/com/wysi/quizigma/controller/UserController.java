c38c7058731b27aba5ff972796b171d01bf9cc17
khoibut <khoibut09@gmail.com>
2024-12-19 10:21:50 +0700

    Enhance JWT authentication filter to provide clearer error messages; remove unused get method in UserController and clean up UserService logic

--- src/main/java/com/wysi/quizigma/controller/UserController.java
+++ src/main/java/com/wysi/quizigma/controller/UserController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,11 +24,6 @@ public class UserController {
 
     public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
-    }
-
-    @GetMapping("/")
-    public String getMethodName() {
-        return "Test";
     }
 
     @PostMapping("/auth")
